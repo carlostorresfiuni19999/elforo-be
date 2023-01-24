23213ac616a1d1aac87b93317bba59496689df51
Carlos Torres <cctc2013@gmail.com>
2023-01-24 10:28:53 -0300

    Correccion del Mapper

--- Helpers/Mappers/QuestionMapper.cs
+++ Helpers/Mappers/QuestionMapper.cs
@@ -10,11 +10,14 @@ namespace elforo_be.Helpers.Mappers
         {
             CreateMap<QuestionDTO, Question>()
                 .ForMember(dest => dest.Person, opt => opt.Ignore())
-                .ForMember(dest => dest.Comments, opt => opt.Ignore());
+                .ForMember(dest => dest.Comments, opt => opt.Ignore())
+                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => DateOnly.FromDateTime(DateTime.Now)));
+                
 
             CreateMap<Question, QuestionDTO>()
                 .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Person == null ? "" : 
                     src.Anonymous ? "Anonimo" : src.Person.Email))
+                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.Date.ToString()))
                 .ForMember(dest => dest.CantComment, opt => opt.MapFrom(src => src.Comments == null ? 0 : src.Comments.Count));
         
         }
