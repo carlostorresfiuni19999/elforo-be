1b4247f389d96040bea7a33c15cd67850cc6150b
Carlos Torres <cctc2013@gmail.com>
2023-01-24 10:33:18 -0300

    Correccion QuestionDTO

--- Models/dto/QuestionDTO.cs
+++ Models/dto/QuestionDTO.cs
@@ -8,6 +8,7 @@ namespace elforo_be.Models.dto
         public String? Date {get; set; }
         public List<String>? Tags {get; set; }
         public int CantComment {get; set; }
+        public String? Question {get; set;}
         
     }
 }
